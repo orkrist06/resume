e2012e5e731ac410fde90e8a6e98113844294032
unknown <amvzero111@gmail.com>
2023-09-17 13:17:35 +0300

    Bootstrap3

--- src/route/index.js
+++ src/route/index.js
@@ -360,6 +360,16 @@ router.get('/bootstrap', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/bootstrap3', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrapLess3', {
+    layout: 'bootstrap'
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 
 router.get('/bio', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
